77b3379ac0c089026f1dfc8028e755e18d310fd1
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-12-24 15:52:16 +0300

    BethanysPieShop created. Implemented dashboard. Data comes from DB using EF. Initial Migration created.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese Cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
