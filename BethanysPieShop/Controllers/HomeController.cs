57f17532ffdfba67cc3e17ff5df0e50b75448cee
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-12-25 17:21:30 +0300

    home page with carousel added

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
