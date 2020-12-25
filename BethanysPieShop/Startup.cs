d927eb287c7c3d8aa0e9bdb35f25b8eb1b0c70c6
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-12-25 18:36:46 +0300

    Shopping cart added

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -30,6 +30,9 @@ namespace BethanysPieShop
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -40,7 +43,9 @@ namespace BethanysPieShop
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
