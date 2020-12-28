ed2a981fc076d8ca4f5844c82342adf2c13848b4
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-12-28 13:40:55 +0300

    CategoryMenu ViewComponent added

---
+++ BethanysPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using System.Linq;
+
+namespace BethanysPieShop.Components
+{
+    public class CategoryMenu: ViewComponent
+    {
+        private readonly ICategoryRepository categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = categoryRepository.AllCategories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
